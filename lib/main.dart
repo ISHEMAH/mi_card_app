aa31b6dc5d30a8a0bf43a817a5f350ed943e3fd9
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-12-29 18:21:08 +0200

    rows

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.teal,
+//         body: SafeArea(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.end,
+//             children: <Widget>[
+//               Container(
+//                 height: 100.0,
+//             width: 100.0,
+//             color: Colors.white,
+//             child:const Text('Hugogo'),
+//           ),
+//           Container(
+//                 height: 100.0,
+//             width: 100.0,
+//             color: Colors.red,
+//             child:const Text('Hugogo'),
+//           ),
+//           Container(
+//                 height: 100.0,
+//             width: 100.0,
+//             color: Colors.yellow,
+//             child:const Text('Hugogo'),
+//           ),
+//             ]
+//           ,)
+//       ),
+//     )
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,32 +51,39 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                width: 100.0,
                 height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child:const Text('Hugogo'),
-          ),
-          Container(
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100.0,
                 height: 100.0,
-            width: 100.0,
-            color: Colors.red,
-            child:const Text('Hugogo'),
+                color: const Color.fromARGB(70, 255, 235, 59),
+              ),
+                ],
+              ),
+              
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.blue,
+              ),
+            ],
           ),
-          Container(
-                height: 100.0,
-            width: 100.0,
-            color: Colors.yellow,
-            child:const Text('Hugogo'),
-          ),
-            ]
-          ,)
+        ),
       ),
-    )
     );
   }
 }
-
-  
